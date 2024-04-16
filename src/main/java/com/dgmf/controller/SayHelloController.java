c493cf4b564f5bd7cdf0d0815ab60a6826e7cee9
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-16 08:47:47 +0200

    Spring MVC Controller | Provide HTML Response with StringBuffer

--- src/main/java/com/dgmf/controller/SayHelloController.java
+++ src/main/java/com/dgmf/controller/SayHelloController.java
@@ -12,4 +12,24 @@ public class SayHelloController {
 	public String sayHello() {
 		return "Hello ! What are you learning today ?";
 	}
+	
+	// "say-hello-html" => "Hello ! What are you learning today ?"
+	@RequestMapping("say-hello-html") // http://localhost:8080/say-hello
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<!DOCTYPE html>");
+		sb.append("<html lang=\"en\">");
+		sb.append("<head>");
+		sb.append("<meta charset=\"UTF-8\">");
+		sb.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+		sb.append("<title>My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My First HTML Page with Body - Changed.");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
 }
