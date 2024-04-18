f603f06f2da37017dce2f7c496d06ce0ef72e024
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-18 11:37:33 +0200

    Capturing QueryParams using RequestParam and First Look at Model

--- src/main/java/com/dgmf/controller/LoginController.java
+++ src/main/java/com/dgmf/controller/LoginController.java
@@ -1,12 +1,19 @@
 package com.dgmf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String goToLoginPage() {
+    public String goToLoginPage(
+            @RequestParam String name, ModelMap modelMap
+    ) {
+        modelMap.put("name", name);
+        
+        System.out.println("Request Param is : " + name);
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -6,6 +6,6 @@
     <title>Login Page</title>
 </head>
 <body>
-    <h1>Welcome to the Login Page</h1>
+    <h1>Welcome to the Login Page ${name}</h1>
 </body>
 </html>
