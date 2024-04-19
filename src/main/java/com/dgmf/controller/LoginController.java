9ee332c78922187050bea621c02b3a52f3c7fdc5
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-19 11:55:51 +0200

    Logging with "slf4j.LoggerFactory"

--- src/main/java/com/dgmf/controller/LoginController.java
+++ src/main/java/com/dgmf/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.dgmf.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/login")
     public String goToLoginPage(
             @RequestParam String name, ModelMap modelMap
     ) {
         modelMap.put("name", name);
-        
+
         System.out.println("Request Param is : " + name);
+        logger.debug("Request Param at Debug Level is : {}", name);
+        logger.info("Request Param at Info Level is : {}", name);
+        logger.warn("Request Param at Warn Level is : {}", name);
+
+
         return "login";
     }
 }
