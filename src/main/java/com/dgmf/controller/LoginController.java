1bfc808749f54d438da29f3c20989d53f20a7984
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-04-23 12:32:02 +0200

    Displaying Login Credentials in a JSP using ModelMap

--- src/main/java/com/dgmf/controller/LoginController.java
+++ src/main/java/com/dgmf/controller/LoginController.java
@@ -5,12 +5,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
+    // To Handle GET Request
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String goToLoginPage() {
         return "login";
+    }
+
+    // To Handle POST Request
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String goToWelcomePage(
+            @RequestParam("name") String name,
+            @RequestParam("password") String password,
+            ModelMap modelMap) {
+        modelMap.put("name", name);
+        modelMap.put("password", password);
+
+        return "welcome";
     }
 }
